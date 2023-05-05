874479ed9222bd469d2705e7e6883869e7274c5d
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-05-05 13:00:11 +0200

    fix snack4

--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -9,14 +9,14 @@ public class Snack4 {
 		System.out.println("Scrivi una parola:");
 		String userInput = sc.nextLine();
 		char[] charArray = userInput.toCharArray();
-		Boolean palindroma = true;
 			
-		for (int i = 0; i<charArray.length; i++) {
+		for (int i = 0; i<charArray.length/2; i++) {
 			if (charArray[i] != charArray[charArray.length -1 - i]) {
-				palindroma = false;
+				System.out.println("Non è palindroma");
+				return;
 			}
 		}
 		
-		System.out.println(palindroma ? "E' palindroma" : "Non è palindroma");
+		System.out.println("E' palindroma");
 	}
 }
