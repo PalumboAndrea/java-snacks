3ee2b775848ca8b4c53dd05762a40240ecafa246
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-05-05 10:35:05 +0200

    first commit

---
+++ src/org/lessons/java/snack1.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class snack1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
